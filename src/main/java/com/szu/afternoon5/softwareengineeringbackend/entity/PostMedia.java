ac239f4784192ac03071cafa0a34246b5eb369aa
CorneliaMo <SweetCorneliaMo@163.com>
2025-12-10 23:11:38 +0800

    fix：添加字段指示媒体在帖子中的顺序

--- src/main/java/com/szu/afternoon5/softwareengineeringbackend/entity/PostMedia.java
+++ src/main/java/com/szu/afternoon5/softwareengineeringbackend/entity/PostMedia.java
@@ -24,6 +24,9 @@ public class PostMedia {
 
     private MediaType mediaType;
 
+    // 用于指示媒体在帖子中的顺序
+    private Integer sortOrder;
+
     public enum MediaType {
         image, video
     }
@@ -31,11 +34,12 @@ public class PostMedia {
     /**
      * 创建媒体记录的构造器，存储关联帖子与上传者信息。
      */
-    public PostMedia(Long postId, Long uploadUserId, String mediaUrl, MediaType mediaType) {
+    public PostMedia(Long postId, Long uploadUserId, String mediaUrl, MediaType mediaType, Integer sortOrder) {
         this.mediaId = null;
         this.postId = postId;
         this.uploadUserId = uploadUserId;
         this.mediaUrl = mediaUrl;
         this.mediaType = mediaType;
+        this.sortOrder = sortOrder;
     }
 }
